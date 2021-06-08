d970f198c04a37b6733ee33d5a5cdef1a738cb3d
Damy Metzke <damy.metzke@gmail.com>
2021-06-08 15:44:29 +0200

    Implement model relations

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Event;
+
 class Registration extends Model
 {
     use HasFactory;
 
     protected $fillable = ["event_id", "name", "email", "note"];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
