96e18139c871dfab346f43fa19bdfe12858d477c
Damy Metzke <damy.metzke@gmail.com>
2021-06-08 14:44:37 +0200

    Add `EventFactory`

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "organizer" => $this->faker->company(),
+            "date" => $this->faker->dateTimeBetween("+1 day", "+20 days"),
+            "location" => $this->faker->words(2, true),
+            "description" => $this->faker->paragraph(20),
+            "max_registration_num" => $this->faker->numberBetween(1, 12) * 5,
+        ];
+    }
+}
