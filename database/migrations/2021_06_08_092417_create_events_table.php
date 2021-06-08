a346aaf3289120f63fc6438a6de6944a6bf379b1
Damy Metzke <damy.metzke@gmail.com>
2021-06-08 11:41:16 +0200

    Implement model and migration for events

---
+++ database/migrations/2021_06_08_092417_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("events", function (Blueprint $table) {
+            $table->id();
+
+            $table->string("organizer", 30);
+            $table->timestamp("date");
+            $table->string("location", 50);
+            $table->text("description");
+            $table->unsignedInteger("max_registration_num")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("events");
+    }
+}
