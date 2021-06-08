30729f896c501226017506c09392a3a8be3ee1ac
Damy Metzke <damy.metzke@gmail.com>
2021-06-08 13:44:50 +0200

    Add `admin.dashboard` view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
      */
     public function dashboard(Request $request)
     {
-        //
+        return view("admin.dashboard");
     }
 
     public function login()
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.app")
+
+@section("content")
+
+<h1>dashboard</h1>
+@guest
+<a href="{{route("admin.login")}}"><button>login</button></a>
+@else
+<form method="post" action="{{route("admin.logout")}}">
+    @csrf
+    <button type="submit">logout</button>
+</form>
+@endguest
+
+@endsection
