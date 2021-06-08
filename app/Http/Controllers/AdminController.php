de4911a765cde0eaf0e54b638de9d76d8bf7263b
Damy Metzke <damy.metzke@gmail.com>
2021-06-08 13:53:21 +0200

    Update `auth.login` view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
 
     public function login()
     {
-        return view("auth/login");
+        return view("admin/login");
     }
 
     public function authenticate(Request $request)
