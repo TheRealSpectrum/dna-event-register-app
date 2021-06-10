7ecb3ef57885fe8d9572af401e6f827769c17f6d
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-06-10 13:37:55 +0200

    Fix padding redirect button

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,8 +84,11 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect()
+            ->route("users.index")
+            ->with("success", "Gebruiker is succesvol verwijderd");
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
 
 @section("content")
 <section class="h-screen bg-gray-100 bg-opacity-50">
-    <div class="px-4 pb-4 text-gray-500 mt-4 md:w-1/6">
+    <div class="px-4 pb-4 text-gray-500 pt-4 md:w-1/6">
         <a href="/gebruikers">
             <button type="submit" class="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                 Terug naar gebruikers   
