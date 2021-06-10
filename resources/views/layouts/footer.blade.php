238c6548cb0ccdda96df1ae543a91fa08dc3767e
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-06-10 10:44:35 +0200

    Change copyright name

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="bg-white dark:bg-gray-800 w-full">
     <div class="max-w-screen-xl mx-auto px-4">
         <div class="text-center text-gray-500 dark:text-gray-200 pt-6 sm:pt-6 font-light flex items-center justify-start">
-            Copyright © {{ date('Y') }} TeamDNA
+            Copyright © {{ date('Y') }} PartyPlanner
         </div>
     </div>
 </div>
