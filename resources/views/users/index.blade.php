d6348679fd32290a6fb1b41995721ceaa3c96c46
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-06-10 14:32:21 +0200

    Add redirect to create user button

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,6 +3,13 @@
 @section("title", "Gebruikers")
 
 @section("content")
+<div class="px-4 pb-4 text-gray-500 pt-4 md:w-1/6">
+    <a href="{{ route("users.create") }}">
+        <button type="button" class="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+            Maak gebruiker aan
+        </button>
+    </a>
+</div>
 <div class="container mx-auto px-4 sm:px-8 max-w-3xl">
     <div class="py-8">
         <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
