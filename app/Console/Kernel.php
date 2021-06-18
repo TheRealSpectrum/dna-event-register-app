8c7e838d00a25022d6c9cb11795e8b958ab17a73
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-06-18 20:18:21 +0200

    Write SendEventNotifcation scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule
+            ->command("send:notification")
+            ->timezone("Europe/Amsterdam")
+            ->dailyAt("12:00");
     }
 
     /**
