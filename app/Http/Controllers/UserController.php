3a3411a7d2f30d300a844c39b2a468f8b11e6102
Damy Metzke <damy.metzke@gmail.com>
2021-06-22 15:48:57 +0200

    Refactor `UserController`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,84 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
+
 use App\Http\Requests\{StoreUserRequest, UpdateUserRequest};
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): View
     {
         return view("users.index", [
             "users" => User::all(),
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(): View
     {
         return view("users.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreUserRequest $request)
+    public function store(StoreUserRequest $request): RedirectResponse
     {
-        $user = User::create($request->transformValidated());
+        User::create($request->transformValidated());
 
         return redirect()
             ->route("users.index")
             ->with("success", "Gebruiker is succesvol aangemaakt");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show($id): View
     {
         return view("users.show", [
             "user" => User::findOrFail($id),
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit($id): View
     {
         return view("users.edit", [
             "user" => User::findOrFail($id),
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateUserRequest $request, User $user)
-    {
+    public function update(
+        UpdateUserRequest $request,
+        User $user
+    ): RedirectResponse {
         if (!Hash::check($request->current_password, $user->password)) {
             return redirect()
                 ->route("users.edit", $user->id)
@@ -88,8 +58,6 @@ class UserController extends Controller
                 ]);
         }
 
-        // dd($request->transformValidated());
-
         $user->update($request->transformValidated());
 
         return redirect()
@@ -97,13 +65,7 @@ class UserController extends Controller
             ->with("success", "Gebruiker is succesvol geupdate");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
+    public function destroy(User $user): RedirectResponse
     {
         $user->delete();
         return redirect()
