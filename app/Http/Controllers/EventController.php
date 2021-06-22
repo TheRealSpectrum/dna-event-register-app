dd00eb32e96f596b7d15942ba935ba52092f3cec
Damy Metzke <damy.metzke@gmail.com>
2021-06-22 15:32:43 +0200

    Refactor `EventController`

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,19 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Event;
-use App\Http\Requests\EventRequest;
-use Carbon\Carbon;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\URL;
+
+use Carbon\Carbon;
+
+use App\Http\Requests\EventRequest;
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): View
     {
         return view("events.index", [
             "events" => Event::withCount("registrations")
@@ -24,23 +23,12 @@ class EventController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(): View
     {
         return view("events.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(EventRequest $request)
+    public function store(EventRequest $request): RedirectResponse
     {
         $createdEvent = Event::create($request->transformed());
 
@@ -49,41 +37,24 @@ class EventController extends Controller
         ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Event $event)
+    public function show(Event $event): View
     {
         return view("events.show", [
             "event" => $event,
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Event $event)
+    public function edit(Event $event): View
     {
         return view("events.edit", [
             "event" => $event,
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(EventRequest $request, Event $event)
-    {
+    public function update(
+        EventRequest $request,
+        Event $event
+    ): RedirectResponse {
         $event->fill($request->transformed());
 
         $event->save();
@@ -91,13 +62,7 @@ class EventController extends Controller
         return redirect()->route("events.show", ["event" => $event->id]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Event $event)
+    public function destroy(Event $event): RedirectResponse
     {
         $event->registrations()->delete();
         $event->delete();
