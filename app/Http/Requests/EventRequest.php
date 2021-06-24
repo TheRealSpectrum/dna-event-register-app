664da1a9b036c205d1d2ede15d009fa00b1a42a7
Damy Metzke <damy.metzke@gmail.com>
2021-06-24 11:06:11 +0200

    Refactor `EventRequest`

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -2,27 +2,18 @@
 
 namespace App\Http\Requests;
 
-use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
+
+use Carbon\Carbon;
 
 class EventRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
             "title" => "required",
